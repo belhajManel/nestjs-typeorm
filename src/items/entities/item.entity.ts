cdafd70f302f78cf02d06f24501662b108d8ead2
belhajManel <belhaj343@gmail.com>
2024-05-01 12:16:35 +0100

    Basic relations

---
+++ src/items/entities/item.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column({ default: true })
+  public: boolean;
+
+  constructor(item: Partial<Item>) {
+    console.log('item ', item);
+    Object.assign(this, item);
+  }
+}
